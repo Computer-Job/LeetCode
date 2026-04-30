a1d4bd316148fb617bf8af3747e194768e50b9bb
Computer-Job <mfrey5@my.gcu.edu>
2026-04-30 16:12:06 -0700

    Added starter file to relearn C++

---
+++ Easy/1-twosum/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+        
+//     }
+// };
+
+int main () {
+    cout << "Hello World";
+
+    return 0;
+}
