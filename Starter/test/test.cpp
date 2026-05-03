865c4164f81c10278bdf1aac8868e040fd2ed47b
Computer-Job <ComputerJob@gmail.com>
2026-05-03 03:02:52 -0700

    Day 3 progress part 2 progress

--- Starter/test/test.cpp
+++ Starter/test/test.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <vector>
 
+#ifndef NDEBUG
+void debugTest() {
+    std::cout << "response" << "\n";
+}
+#endif
+
 int main () {
     std::cout << "The byte size of an int: " << sizeof(int) << "\n";
     std::cout << "The byte size of an char: " << sizeof(char) << "\n";
     std::cout << "The byte size of an double: " << sizeof(double) << "\n";
     std::cout << "The byte size of an float: " << sizeof(float) << "\n";
+    std::cout << "The byte size of a size_t: " << sizeof(std::size_t) << "\n";
+    std::cout << "The byte size of a long: " << sizeof(long) << "\n";
 
-    std::vector<int> values;
+    std::vector<int> values {};
+
+    std::cout << "The byte size of the vector: " << sizeof(values) << "\n";
 
     values.push_back(1);
     values.push_back(2);
@@ -15,11 +25,19 @@ int main () {
     values.push_back(4);
     values.push_back(5);
 
-    std::cout << "The byte size of the vector: " << sizeof(values) << "\n";
+    std::cout << "The byte size of the vector now is: " << sizeof(values) << "\n";
 
-    // for (int i = 0; i < sizeof(values); i ++) {
-    //     std::cout << values. << "\n";
-    // }
+    for (const int& i : values) {
+        std::cout << i << "\n";
+    }
+
+    for (std::size_t i = 0; i < values.size(); i ++) {
+        std::cout << values[i] << "\n";
+    }
+
+    #ifndef NDEBUG
+        debugTest();
+    #endif
 
     return 0;
 }
